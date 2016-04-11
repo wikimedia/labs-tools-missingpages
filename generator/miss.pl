952ebebe8b670c71eed3cdcb13e8cfd65fede4be
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-04-11 20:36:54 +0200

    Update script

--- generator/miss.pl
+++ generator/miss.pl
@@ -10,16 +10,16 @@ $date = <X>;
 close X;
 $date =~ s/ /&nbsp;/g;
 open O, ">missl";
-print O <<EOF;
-<!--
- Tento seznam je generován skriptem a čas od času je automaticky přepsán.
- Needitujte ho, vaše změny budou přepsány
- Editovat lze podstránky /head (hlavička) a /tail (patička)
- Tyto jsou do stránky vkládány a jejich obsah není skriptem nijak měněn.
- -->
-{{/head|$date}}
-<ol>
-EOF
+#print O <<EOF;
+#<!--
+# Tento seznam je generován skriptem a čas od času je automaticky přepsán.
+# Needitujte ho, vaše změny budou přepsány
+# Editovat lze podstránky /head (hlavička) a /tail (patička)
+# Tyto jsou do stránky vkládány a jejich obsah není skriptem nijak měněn.
+# -->
+#{{/head|$date}}
+#<ol>
+#EOF
 
 %q = ();
 
@@ -27,16 +27,18 @@ open X, "<MISSLINK.txt";
 while (<X>) {
     if (/^(\d+)\t(.*)/) {
         $n = $1;
-        next if ( $n < 6 );
+        next if ( $n < 1 ); # vygeneruje, pokud je alespoň 1 výskyt. Pro alespoň dva výskyty použít $n < 2
         $no   = $n;
         $v    = $2;
-        $nkey = 1000000 - $n;
+        $nkey = 1100000 - $n;
         $k    = "$nkey/$v";
 
         #$q{$k} = "# [[$v]] ($no)\n";
         #$q{$k} = "{{subst:Stránka|$v|<li>| ($no)</li>}}";
-        $q{$k} = "{{#ifexist:$v | |<li>[[$v]] ([[Special:Whatlinkshere/$v|$no odkazů]])</li>\n}}";
-    }
+        #$q{$k} = "{{#ifexist:$v | |<li>[[$v]] ([[Special:Whatlinkshere/$v|$no odkazů]])</li>\n}}";
+        #$q{$k} = "{{Stránka|$v|<li>|</li>}}\n"; 
+        $q{$k} = "$v\n"; #toto je aktuální výstup
+    }    
 }
 close X;
 $iii = 0;
@@ -44,11 +46,10 @@ $iii = 0;
 foreach my $i (@kk) {
     $iii++;
     print O $q{$i};
-    last if ( $iii == 1000 );
+    #last if ( $iii == 1100000 ); #1100000 je kvůli tomu, že generovaných odkazů už je přes milion
 }
 
 print O <<EOF;
 </ol>
 {{/tail}}
 EOF
-
