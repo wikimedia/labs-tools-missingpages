fe1f73afaf2b6702a76842feb143b7b08674b8c4
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-04-12 21:24:21 +0200

    hotfix

--- public/process.py
+++ public/process.py
@@ -51,13 +51,13 @@ i = 0
 print "<ol>"
 if whatlinkshere:
 	for row in data:
-		print '<li><a href="https://cs.wikipedia.org/wiki/"' + row[0] + '</a> (<a href="https://cs.wikipedia.org/wiki/Special:WhatLinksHere/' + row[0] + '">odkazy</a>)'
+		print '<li><a href="https://cs.wikipedia.org/wiki/' + row[0] + '">' + row[0] + '</a> (<a href="https://cs.wikipedia.org/wiki/Special:WhatLinksHere/' + row[0] + '">odkazy</a>)</li>'
 		i += 1
 		if i > 100:
 			break
 else: 
 	for row in data:
-		print '<li><a href="https://cs.wikipedia.org/wiki/"' + row[0] + '</a>'
+		print '<li><a href="https://cs.wikipedia.org/wiki/' + row[0] + '">' + row[0] + '</a></li>'
 		i += 1
 		if i > 100:
 			break
