fa75798df6ee5d93c0ffad7aa2f3910ad63c772e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-06-18 16:06:41 +0200

    Add sql file which loads missing pages from db

---
+++ database/sql.sql
@@ -0,0 +1,4 @@
+select pl_title as title_to from cswiki_p.pagelinks left join cswiki_p.page on (pl_from=page_id)
+where pl_namespace=0 and (page_namespace in (0, 2, 14, 100)
+or (page_namespace=4 and (page_title like 'Požadované%' or page_title like 'Cizojazyčné%' or page_title like 'WikiProjekt%')))
+and not exists (select * from cswiki_p.page where page_namespace=pl_namespace and page_title=pl_title) group by title_to;
