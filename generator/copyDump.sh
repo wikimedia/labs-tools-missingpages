ca0a2b75abc04ff3d0ceac29f90d0ae55bcbc621
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-03-22 08:48:16 +0100

    Typo

--- generator/copyDump.sh
+++ generator/copyDump.sh
@@ -2,4 +2,4 @@
 
 cp `ls -d /public/dumps/public/cswiki/* | sort -n | head -n1`/cswiki-*-pages-articles.xml.bz2 .
 mv cswiki-*-pages-articles.xml.bz2 cswiki-latest-pages-articles.xml.bz2
-bzip2 -d cswiki-lastest-pages-articles.xml.bz2
+bzip2 -d cswiki-latest-pages-articles.xml.bz2
