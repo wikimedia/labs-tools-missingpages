ae83ed77cd15b22f731b4f09778a85007ecaf11a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-03-24 21:57:41 +0100

    Remove old generator files

--- generator/copyDump.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp `ls -d /public/dumps/public/cswiki/* | sort -n | head -n1`/cswiki-*-pages-articles.xml.bz2 .
-mv cswiki-*-pages-articles.xml.bz2 cswiki-latest-pages-articles.xml.bz2
-bzip2 -d cswiki-latest-pages-articles.xml.bz2
